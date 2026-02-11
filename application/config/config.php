98c9182b2ee23ac9174f207b5ecd310b6a048f13
patrickProgg <patrickhenrybersaluna115@gmail.com>
2026-02-11 10:51:54 +0800

    t

--- application/config/config.php
+++ application/config/config.php
@@ -389,14 +389,30 @@ $config['encryption_key'] = '892a1c4865d4bb8b3936fb16ede01dee4ee1a7be0cb58f370ab
 | except for 'cookie_prefix' and 'cookie_httponly', which are ignored here.
 |
 */
+// $config['sess_driver'] = 'files';
+// $config['sess_cookie_name'] = 'ci_session';
+// $config['sess_samesite'] = 'Lax';
+// $config['sess_expiration'] = 7200;
+// $config['sess_save_path'] = NULL;
+// $config['sess_match_ip'] = FALSE;
+// $config['sess_time_to_update'] = 300;
+// $config['sess_regenerate_destroy'] = FALSE;
+
+
 $config['sess_driver'] = 'files';
 $config['sess_cookie_name'] = 'ci_session';
-$config['sess_samesite'] = 'Lax';
 $config['sess_expiration'] = 7200;
-$config['sess_save_path'] = NULL;
+$config['sess_save_path'] = sys_get_temp_dir(); // For AlwaysData
 $config['sess_match_ip'] = FALSE;
 $config['sess_time_to_update'] = 300;
 $config['sess_regenerate_destroy'] = FALSE;
+
+// ADD THESE NEW SETTINGS to fix deprecation:
+$config['sess_match_useragent'] = TRUE;
+$config['sess_encrypt_cookie'] = FALSE;
+$config['sess_use_database'] = FALSE;
+$config['sess_table_name'] = 'ci_sessions';
+$config['sess_valid_drivers'] = array('files', 'database');
 
 /*
 |--------------------------------------------------------------------------
